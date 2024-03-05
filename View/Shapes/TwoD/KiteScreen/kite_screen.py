2a395bddaa79d28e67e3e0b08680a18b4f986ee3
Navadeep <69576886+Nandu45321@users.noreply.github.com>
2024-03-05 23:05:21 +0530

    Editted kite_screen.py

--- View/Shapes/TwoD/KiteScreen/kite_screen.py
+++ View/Shapes/TwoD/KiteScreen/kite_screen.py
@@ -1,5 +1,5 @@
 from kivy.uix.screenmanager import Screen
-from kivymd.uix.snackbar import Snackbar
+from kivymd.uix.snackbar import MDSnackbar
 
 
 class KiteScreenView(Screen):
@@ -9,10 +9,10 @@ class KiteScreenView(Screen):
         side_1_input = self.ids.side_1_value.text
         side_2_input = self.ids.side_2_value.text
         if diagonal_1_input != '' and diagonal_2_input != '' and side_1_input != '' and side_2_input != '':
-            self.ids.area_value.hint_text = str("{:.3f}".format((float(diagonal_1_input) * float(diagonal_2_input)) / 2))
+            self.ids.area_value.hint_text = str(
+                "{:.3f}".format((float(diagonal_1_input) * float(diagonal_2_input)) / 2))
             self.ids.perimeter_value.hint_text = str("{:.3f}".format(2 * (float(side_1_input) + float(side_2_input))))
         else:
             self.ids.area_value.hint_text = ''
             self.ids.perimeter_value.hint_text = ''
-            Snackbar(text="[color=#ff6961]Please fill the all the required blanks[/color]", ).open()
-
+            MDSnackbar(text="[color=#ff6961]Please fill the all the required blanks[/color]", ).open()
