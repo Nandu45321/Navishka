6a14a3587ade64cd37a93d589a6350db4580959f
Navadeep <69576886+Nandu45321@users.noreply.github.com>
2024-03-05 22:58:33 +0530

    Edit quadrilateral_screen.py

--- View/Shapes/TwoD/QuadrilateralScreen/quadrilateral_screen.py
+++ View/Shapes/TwoD/QuadrilateralScreen/quadrilateral_screen.py
@@ -1,5 +1,5 @@
 from kivy.uix.screenmanager import Screen
-from kivymd.uix.snackbar import Snackbar
+from kivymd.uix.snackbar import MDSnackbar
 
 
 class QuadrilateralScreenView(Screen):
@@ -8,8 +8,8 @@ class QuadrilateralScreenView(Screen):
         perpendicular_height_1_input = self.ids.perpendicular_height_1_value.text
         perpendicular_height_2_input = self.ids.perpendicular_height_2_value.text
         if diagonal_input != '' and perpendicular_height_1_input != '' and perpendicular_height_2_input != '':
-            self.ids.area_value.hint_text = str("{:.3f}".format((1 / 2 * float(diagonal_input) * (float(perpendicular_height_1_input) + float(perpendicular_height_2_input)))))
+            self.ids.area_value.hint_text = str("{:.3f}".format((1 / 2 * float(diagonal_input) * (
+                        float(perpendicular_height_1_input) + float(perpendicular_height_2_input)))))
         else:
             self.ids.area_value.hint_text = ''
-            Snackbar(text="[color=#ff6961]Please fill the all the required blanks[/color]", ).open()
-
+            MDSnackbar(text="[color=#ff6961]Please fill the all the required blanks[/color]", ).open()
