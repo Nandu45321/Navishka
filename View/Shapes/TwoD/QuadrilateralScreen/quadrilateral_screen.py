0d38e8f5a5d23c4725e1429ba46d19fc15d837a5
Navadeep <69576886+Nandu45321@users.noreply.github.com>
2024-03-16 03:57:05 +0530

    Refurbished Quadrilateral, Scalene Triangle

--- View/Shapes/TwoD/QuadrilateralScreen/quadrilateral_screen.py
+++ View/Shapes/TwoD/QuadrilateralScreen/quadrilateral_screen.py
@@ -1,15 +1,28 @@
+from kivy.metrics import dp
 from kivy.uix.screenmanager import Screen
-from kivymd.uix.snackbar import MDSnackbar
+from kivymd.uix.snackbar import MDSnackbar, MDSnackbarText
 
 
 class QuadrilateralScreenView(Screen):
     def queans(self):
-        diagonal_input = self.ids.diagonal_value.text
-        perpendicular_height_1_input = self.ids.perpendicular_height_1_value.text
-        perpendicular_height_2_input = self.ids.perpendicular_height_2_value.text
-        if diagonal_input != '' and perpendicular_height_1_input != '' and perpendicular_height_2_input != '':
-            self.ids.area_value.text = str("{:.3f}".format((1 / 2 * float(diagonal_input) * (
-                    float(perpendicular_height_1_input) + float(perpendicular_height_2_input)))))
+        diagonal = self.ids.diagonal_value.text
+        perpendicular_height_1 = self.ids.perpendicular_height_1_value.text
+        perpendicular_height_2 = self.ids.perpendicular_height_2_value.text
+        if diagonal != '' and perpendicular_height_1 != '' and perpendicular_height_2 != '':
+            diagonal = float(diagonal)
+            perpendicular_height_1 = float(perpendicular_height_1)
+            perpendicular_height_2 = float(perpendicular_height_2)
+
+            area = 1 / 2 * diagonal * (perpendicular_height_1 + perpendicular_height_2)
+
+            self.ids.area_value.text = str(round(area, 3))
         else:
             self.ids.area_value.text = ''
-            MDSnackbar(text="[color=#ff6961]Please fill the all the required blanks[/color]", ).open()
+            MDSnackbar(
+                MDSnackbarText(
+                    text="Please fill the all the required blanks",
+                ),
+                y=dp(24),
+                pos_hint={"center_x": 0.5},
+                size_hint_x=0.95,
+            ).open()
--- View/Shapes/TwoD/ScaleneTriangleScreen/scalene_triangle_screen.py
+++ View/Shapes/TwoD/ScaleneTriangleScreen/scalene_triangle_screen.py
@@ -1,20 +1,33 @@
+from kivy.metrics import dp
 from kivy.uix.screenmanager import Screen
-from kivymd.uix.snackbar import MDSnackbar
+from kivymd.uix.snackbar import MDSnackbar, MDSnackbarText
 
 
 class ScaleneTriangleScreenView(Screen):
     def queans(self):
-        side_1_input = self.ids.side_1_value.text
-        side_2_input = self.ids.side_2_value.text
-        side_3_input = self.ids.side_3_value.text
-        if side_1_input != '' and side_2_input != '' and side_3_input != '':
-            semi_perimeter = (float(side_1_input) + float(side_2_input) + float(side_3_input)) / 2
-            self.ids.area_value.text = str("{:.3f}".format((semi_perimeter * (
-                    semi_perimeter - float(side_1_input)) * (semi_perimeter - float(side_2_input)) * (
-                                                                    semi_perimeter - float(side_3_input))) ** (
-                                                                   1 / 2)))
-            self.ids.perimeter_value.text = str("{:.3f}".format(semi_perimeter * 2))
+        side_1 = self.ids.side_1_value.text
+        side_2 = self.ids.side_2_value.text
+        side_3 = self.ids.side_3_value.text
+        if side_1 != '' and side_2 != '' and side_3 != '':
+            side_1 = float(side_1)
+            side_2 = float(side_2)
+            side_3 = float(side_3)
+
+            semi_perimeter = (side_1 + side_2 + side_3) / 2
+            area = (semi_perimeter * (semi_perimeter - side_1) * (semi_perimeter - side_2) * (
+                    semi_perimeter - side_3)) ** (1 / 2)
+            perimeter = semi_perimeter * 2
+
+            self.ids.area_value.text = str(round(area, 3))
+            self.ids.perimeter_value.text = str(round(perimeter, 3))
         else:
             self.ids.area_value.text = ''
             self.ids.perimeter_value.text = ''
-            MDSnackbar(text="[color=#ff6961]Please fill the all the required blanks[/color]", ).open()
+            MDSnackbar(
+                MDSnackbarText(
+                    text="Please fill the all the required blanks",
+                ),
+                y=dp(24),
+                pos_hint={"center_x": 0.5},
+                size_hint_x=0.95,
+            ).open()
