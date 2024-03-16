5b7065e83e56584c501d1278132d354a29b70b07
Navadeep <69576886+Nandu45321@users.noreply.github.com>
2024-03-16 21:11:10 +0530

    Refurbished Parallelogram

--- View/Shapes/TwoD/ParallelogramScreen/parallelogram_screen.py
+++ View/Shapes/TwoD/ParallelogramScreen/parallelogram_screen.py
@@ -1,27 +1,39 @@
+from kivy.metrics import dp
 from kivy.uix.screenmanager import Screen
-from kivymd.uix.snackbar import MDSnackbar
+from kivymd.uix.snackbar import MDSnackbar, MDSnackbarText
 
 
 class ParallelogramScreenView(Screen):
     def queans(self):
-        height_input = self.ids.height_value.text
-        base_input = self.ids.base_value.text
-        side_input = self.ids.side_value.text
-        if height_input != '' and base_input != '' and side_input != '':
-            self.ids.area_value.text = str("{:.3f}".format(float(base_input) * float(height_input)))
-            self.ids.perimeter_value.text = str("{:.3f}".format(2 * (float(side_input) + float(base_input))))
-            if side_input != '0':  # s**2 + h**2 + 2*h*s*((s**2 - h**2)**(1 / 2) / s)
-                self.ids.diagonal_1.text = str("{:.3f}".format((float(side_input) ** 2 + float(
-                    height_input) ** 2 + 2 * float(height_input) * float(side_input) * ((float(side_input) ** 2 - float(
-                    height_input) ** 2) ** (1 / 2) / float(side_input))) ** (1 / 2)))
-                self.ids.diagonal_2.text = str("{:.3f}".format((float(side_input) ** 2 + float(
-                    height_input) ** 2 - 2 * float(height_input) * float(side_input) * ((float(side_input) ** 2 - float(
-                    height_input) ** 2) ** (1 / 2) / float(side_input))) ** (1 / 2)))
-            else:
-                self.ids.perimeter_value.text = ''
-                self.ids.diagonal_1.text = ''
-                self.ids.diagonal_2.text = ''
-                MDSnackbar(text="[color=#ff6961]Please fill all the blanks for diagonals[/color]", ).open()
+        height = self.ids.height_value.text
+        base = self.ids.base_value.text
+        side = self.ids.side_value.text
+        if height != '' and base != '' and side != '' and float(side) >= float(height):
+            height = float(height)
+            base = float(base)
+            side = float(side)
+
+            area = base * height
+            perimeter = 2 * (side + base)
+            diagonal_1 = (side ** 2 + height ** 2 + 2 * height * side * (
+                    (side ** 2 - height ** 2) ** (1 / 2) / side)) ** (1 / 2)
+            diagonal_2 = (side ** 2 + height ** 2 - 2 * height * side * (
+                    (side ** 2 - height ** 2) ** (1 / 2) / side)) ** (1 / 2)
+
+            self.ids.area_value.text = str(round(area))
+            self.ids.perimeter_value.text = str(round(perimeter))
+            self.ids.diagonal_1.text = str(round(diagonal_1))
+            self.ids.diagonal_2.text = str(round(diagonal_2))
         else:
             self.ids.area_value.text = ''
-            MDSnackbar(text="[color=#ff6961]Please fill all the required blanks[/color]", ).open()
+            self.ids.perimeter_value.text = ''
+            self.ids.diagonal_1.text = ''
+            self.ids.diagonal_2.text = ''
+            MDSnackbar(
+                MDSnackbarText(
+                    text="Please enter valid inputs",
+                ),
+                y=dp(24),
+                pos_hint={"center_x": 0.5},
+                size_hint_x=0.95,
+            ).open()
