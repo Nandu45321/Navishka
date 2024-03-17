56f046c9bae2a276ea88ab2cd74bfc8189c8b353
Navadeep <69576886+Nandu45321@users.noreply.github.com>
2024-03-17 20:36:47 +0530

    Refurbished Rectangle

--- View/Shapes/TwoD/RectangleScreen/rectangle_screen.py
+++ View/Shapes/TwoD/RectangleScreen/rectangle_screen.py
@@ -1,18 +1,32 @@
+from kivy.metrics import dp
 from kivy.uix.screenmanager import Screen
-from kivymd.uix.snackbar import MDSnackbar
+from kivymd.uix.snackbar import MDSnackbar, MDSnackbarText
 
 
 class RectangleScreenView(Screen):
     def queans(self):
-        length_input = self.ids.length_value.text
-        breadth_input = self.ids.breadth_value.text
-        if length_input != '' and breadth_input != '':
-            self.ids.area_value.text = str("{:.3f}".format(float(length_input) * float(breadth_input)))
-            self.ids.perimeter_value.text = str("{:.3f}".format(2 * (float(length_input) + float(breadth_input))))
-            self.ids.diagonal_value.text = str(
-                "{:.3f}".format((float(length_input) ** 2 + float(breadth_input) ** 2) ** (1 / 2)))
+        length = self.ids.length_value.text
+        breadth = self.ids.breadth_value.text
+        if length != '' and breadth != '':
+            length = float(length)
+            breadth = float(breadth)
+
+            area = length * breadth
+            perimeter = 2 * (length + breadth)
+            diagonal = (length ** 2 + breadth ** 2) ** (1 / 2)
+
+            self.ids.area_value.text = str(round(area, 3))
+            self.ids.perimeter_value.text = str(round(perimeter, 3))
+            self.ids.diagonal_value.text = str(round(diagonal, 3))
         else:
             self.ids.area_value.text = ''
             self.ids.perimeter_value.text = ''
             self.ids.diagonal_value.text = ''
-            MDSnackbar(text="[color=#ff6961]Please fill all the required blanks[/color]", ).open()
+            MDSnackbar(
+                MDSnackbarText(
+                    text="Please fill all the required blanks",
+                ),
+                y=dp(24),
+                pos_hint={"center_x": 0.5},
+                size_hint_x=0.95,
+            ).open()
