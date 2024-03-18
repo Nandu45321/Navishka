17727e0ce3d57fb641dabea49f71bf656006301e
Navadeep <69576886+Nandu45321@users.noreply.github.com>
2024-03-18 21:10:15 +0530

    Refurbished Kite

--- View/Shapes/TwoD/KiteScreen/kite_screen.py
+++ View/Shapes/TwoD/KiteScreen/kite_screen.py
@@ -1,5 +1,6 @@
+from kivy.metrics import dp
 from kivy.uix.screenmanager import Screen
-from kivymd.uix.snackbar import MDSnackbar
+from kivymd.uix.snackbar import MDSnackbar, MDSnackbarText
 
 
 class KiteScreenView(Screen):
@@ -9,10 +10,24 @@ class KiteScreenView(Screen):
         side_1 = self.ids.side_1_value.text
         side_2 = self.ids.side_2_value.text
         if diagonal_1 != '' and diagonal_2 != '' and side_1 != '' and side_2 != '':
-            self.ids.area_value.text = str(
-                "{:.3f}".format((float(diagonal_1) * float(diagonal_2)) / 2))
-            self.ids.perimeter_value.text = str("{:.3f}".format(2 * (float(side_1) + float(side_2))))
+            diagonal_1 = float(diagonal_1)
+            diagonal_2 = float(diagonal_2)
+            side_1 = float(side_1)
+            side_2 = float(side_2)
+
+            area = (diagonal_1 * diagonal_2) / 2
+            perimeter = 2 * (side_1 + side_2)
+
+            self.ids.area_value.text = str(round(area, 3))
+            self.ids.perimeter_value.text = str(round(perimeter, 3))
         else:
             self.ids.area_value.text = ''
             self.ids.perimeter_value.text = ''
-            MDSnackbar(text="[color=#ff6961]Please fill all the required blanks[/color]", ).open()
+            MDSnackbar(
+                MDSnackbarText(
+                    text="Please fill all the required blanks",
+                ),
+                y=dp(24),
+                pos_hint={"center_x": 0.5},
+                size_hint_x=0.95,
+            ).open()
