022753d67b690daa7b07f13aa1166bbaa737b1ef
Nandu45321 <69576886+Nandu45321@users.noreply.github.com>
2024-06-01 15:32:09 +0530

    Edit Nonagon

--- View/Shapes/TwoD/RegularNonagonScreen/regular_nonagon_screen.py
+++ View/Shapes/TwoD/RegularNonagonScreen/regular_nonagon_screen.py
@@ -1,14 +1,25 @@
+from kivy.metrics import dp
 from kivy.uix.screenmanager import Screen
-from kivymd.uix.snackbar import MDSnackbar
+from kivymd.uix.snackbar import MDSnackbar, MDSnackbarText
 
 
 class RegularNonagonScreenView(Screen):
     def queans(self):
         side = self.ids.side_value.text
         if side != '':
-            self.ids.area_value.text = str("{:.3f}".format((9 / 4) * (float(side) ** 2) * 2.74747741945462))
-            self.ids.perimeter_value.text = str("{:.3f}".format(9 * float(side)))
+            side = float(side)
+            area = (9 / 4) * (side ** 2) * 2.74747741945462
+            perimeter = 9 * side
+            self.ids.area_value.text = str(round(area, 3))
+            self.ids.perimeter_value.text = str(round(perimeter, 3))
         else:
             self.ids.area_value.text = ''
             self.ids.perimeter_value.text = ''
-            MDSnackbar(text="[color=#ff6961]Please fill all the required blanks[/color]", ).open()
+            MDSnackbar(
+                MDSnackbarText(
+                    text="Please fill all the required blanks",
+                ),
+                y=dp(24),
+                pos_hint={"center_x": 0.5},
+                size_hint_x=0.95,
+            ).open()
