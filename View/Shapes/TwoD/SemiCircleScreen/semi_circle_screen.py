b69a0927cc314b675b05db6274a9b255d646974a
Nandu45321 <69576886+Nandu45321@users.noreply.github.com>
2024-06-01 16:04:51 +0530

    Edit Semi-circle

--- View/Shapes/TwoD/SemiCircleScreen/semi_circle_screen.py
+++ View/Shapes/TwoD/SemiCircleScreen/semi_circle_screen.py
@@ -1,16 +1,25 @@
+from kivy.metrics import dp
 from kivy.uix.screenmanager import Screen
-from kivymd.uix.snackbar import MDSnackbar
+from kivymd.uix.snackbar import MDSnackbar, MDSnackbarText
 
 
 class SemiCircleScreenView(Screen):
     def queans(self):
         radius = self.ids.radius_value.text
         if radius != '':
-            self.ids.area_value.text = str(
-                "{:.3f}".format((1 / 2) * (3.141592653589793238 * (float(radius) ** 2))))
-            self.ids.circumference_value.text = str(
-                "{:.3f}".format((3.141592653589793238 * float(radius)) + (2 * float(radius))))
+            radius = float(radius)
+            area = (3.141592653589793238 * (radius ** 2)) / 2
+            circumference = 3.141592653589793238 * radius + 2 * radius
+            self.ids.area_value.text = str(round(area, 3))
+            self.ids.circumference_value.text = str(round(circumference, 3))
         else:
             self.ids.area_value.text = ''
             self.ids.circumference_value.text = ''
-            MDSnackbar(text="[color=#ff6961]Please fill all the required blanks[/color]", ).open()
+            MDSnackbar(
+                MDSnackbarText(
+                    text="Please fill all the required blanks",
+                ),
+                y=dp(24),
+                pos_hint={"center_x": 0.5},
+                size_hint_x=0.95,
+            ).open()
